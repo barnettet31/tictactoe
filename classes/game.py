a251962e3f29ff147ac7fb86d0f8b1ee6a4bcf79
Travis Barnette <barnette.travis31@gmail.com>
2023-11-22 20:42:10 -0500

    added function definitions for turn logic

--- classes/game.py
+++ classes/game.py
@@ -1,21 +1,66 @@
 from classes.grid_item import GridItem
+
 import pygame
 grid = [
-    GridItem(pygame.Rect(0, 0, 300, 300), "top left"),
-    GridItem(pygame.Rect(311, 0, 300, 300), "top middle"),
-    GridItem(pygame.Rect(611, 0, 300, 300),  "top right"),
-    GridItem(pygame.Rect(0, 311, 300, 300), "middle left"),
-    GridItem(pygame.Rect(311, 311, 300, 300), "middle middle"),
-    GridItem(pygame.Rect(611, 311, 300, 300), "middle right"),
-    GridItem(pygame.Rect(0, 611, 300, 300), "bottom left"),
-    GridItem(pygame.Rect(311, 611, 300, 300),  "bottom middle"),
-    GridItem(pygame.Rect(611, 611, 300, 300), "bottom right"),
+    GridItem(pygame.Rect(0, 0, 300, 300), 0),
+    GridItem(pygame.Rect(311, 0, 300, 300), 1),
+    GridItem(pygame.Rect(611, 0, 300, 300),  2),
+    GridItem(pygame.Rect(0, 311, 300, 300), 3),
+    GridItem(pygame.Rect(311, 311, 300, 300), 4),
+    GridItem(pygame.Rect(611, 311, 300, 300), 5),
+    GridItem(pygame.Rect(0, 611, 300, 300), 6),
+    GridItem(pygame.Rect(311, 611, 300, 300),  7),
+    GridItem(pygame.Rect(611, 611, 300, 300), 8),
 ]
 
 
 class Game():
-    def __init__(self, screen):
+    def __init__(self, screen: pygame.Surface):
         self.screen = screen
-        self.grid = grid
-        self.is_game_over = False
+        self.grid = [
+            GridItem(pygame.Rect(0, 0, 300, 300), 0),
+            GridItem(pygame.Rect(311, 0, 300, 300), 1),
+            GridItem(pygame.Rect(611, 0, 300, 300),  2),
+            GridItem(pygame.Rect(0, 311, 300, 300), 3),
+            GridItem(pygame.Rect(311, 311, 300, 300), 4),
+            GridItem(pygame.Rect(611, 311, 300, 300), 5),
+            GridItem(pygame.Rect(0, 611, 300, 300), 6),
+            GridItem(pygame.Rect(311, 611, 300, 300),  7),
+            GridItem(pygame.Rect(611, 611, 300, 300), 8),
+        ]
         self.turn = 1
+        self.winner = None
+
+    def take_turn(self, pos):
+        box = None
+        for grid_element in self.grid:
+            if grid_element.rect.collidepoint(pos):
+                box = grid_element
+                break
+        # user going to click on screen
+        # if box is checked, shake screen
+        # else set box to checked and rerender screen
+        # check winner, if winner show alert to reset with winner
+        if box is None:
+            pass
+        if box.checked:
+            print("Taken")
+            pass
+        else:
+            box.set_checked(self.turn)
+            image = pygame.image.load('./x.png') if self.turn == 1 else pygame.image.load('./o.png')
+            self.screen.blit(pygame.transform.scale(image, box.rect[2:]), box.rect[:2])
+            pygame.display.flip()
+    def check_winner(self):
+        return False
+        winning_lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [
+            0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+        # loops through all the squares and checks for winning combinaitions
+    def change_turn(self):
+        if self.turn == 1:
+            self.turn = 2
+            print("Changing to player 2")
+        else:
+            print("Changing to player 1")
+            self.turn = 1
